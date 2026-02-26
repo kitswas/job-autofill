f3a2c4aad3cf1d5895ad0deb8b2a02fa5ef0c578
kitswas <90329875+kitswas@users.noreply.github.com>
2026-02-26 11:10:11 +0530

    feat: update build process to modify manifest version in test mode

--- apps/extension/build.js
+++ apps/extension/build.js
@@ -1,5 +1,5 @@
 import { build } from "esbuild";
-import { mkdir, copyFile, cp, rm } from "fs/promises";
+import { mkdir, copyFile, cp, rm, readFile, writeFile } from "fs/promises";
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -27,7 +27,14 @@ await build({
 	},
 });
 
-await copyFile(path.join(__dirname, "manifest.json"), path.join(distDir, "manifest.json"));
+const manifestSrc = path.join(__dirname, "manifest.json");
+const manifestDst = path.join(distDir, "manifest.json");
+const manifestRaw = await readFile(manifestSrc, "utf8");
+const manifest = JSON.parse(manifestRaw);
+if (isTest) {
+	manifest.version = "999.999.999";
+}
+await writeFile(manifestDst, JSON.stringify(manifest, null, 2));
 await copyFile(path.join(__dirname, "../../LICENSE"), path.join(distDir, "LICENSE"));
 await cp(path.join(__dirname, "icons"), path.join(distDir, "icons"), { recursive: true });
 
